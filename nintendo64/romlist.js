5ffdbcfa3621cf9e742617223422db16b1dbd891
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2023-11-01 22:34:59 -0400

    no more ruffle and addition of n64

---
+++ nintendo64/romlist.js
@@ -0,0 +1,8 @@
+var ROMLIST = [
+    /*
+    {url:"roms/mario.z64",title:"Super Mario 64"},
+    {url:"roms/mariokart.z64",title:"Mario Kart 64"},
+    {url:"roms/kirby64.z64",title:"Kirby 64: The Crystal Shards"},
+	{url:"roms/papermario.z64",title:"Paper Mario"},
+    */
+];
