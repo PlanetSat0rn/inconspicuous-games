9d660a40ec8117f74426317dbb40b7b0a7e798c2
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2023-11-01 23:22:47 -0400

    Update romlist.js

--- nintendo64/romlist.js
+++ nintendo64/romlist.js
@@ -1,6 +1,6 @@
 var ROMLIST = [
-    {url:"https://planetsat0rn.github.io/inconspicuous-games/nintendo64/roms/mario.z64",title:"Super Mario 64"},
-    {url:"https://planetsat0rn.github.io/inconspicuous-games/nintendo64/roms/mariokart.z64",title:"Mario Kart 64"},
-    {url:"https://planetsat0rn.github.io/inconspicuous-games/nintendo64/roms/kirby64.z64",title:"Kirby 64: The Crystal Shards"},
-	{url:"https://planetsat0rn.github.io/inconspicuous-games/nintendo64/roms/papermario.z64",title:"Paper Mario"},
+    {url:"https://planetsat0rn.github.io/thezone/nintendo64/roms/mario.z64",title:"Super Mario 64"},
+    {url:"https://planetsat0rn.github.io/thezone/nintendo64/roms/mariokart.z64",title:"Mario Kart 64"},
+    {url:"https://planetsat0rn.github.io/thezone/nintendo64/roms/kirby64.z64",title:"Kirby 64: The Crystal Shards"},
+	{url:"https://planetsat0rn.github.io/thezone/nintendo64/roms/papermario.z64",title:"Paper Mario"},
 ];
